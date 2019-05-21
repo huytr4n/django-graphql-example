08e1fa660fa5626ff9e8044e67192bcf9d13147e
Huy Tran <huy.tranquoc@asnet.com.vn>
2019-05-21 16:41:58 +0700

    Push all code

---
+++ movies/README.md
@@ -0,0 +1,22 @@
+# Django GraphQL
+
+## Installation
+
+```bash
+
+pipenv install
+
+```
+
+## Up and runing
+
+```bash
+
+pipenv shell
+
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+
+```
+
+Go to [GraphQL Interface](http://localhost:8000/graphql/) for the result.
---
+++ movies/models.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ('name',)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    actors = models.ManyToManyField(Actor)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ('title',)
